6d6d2a2797a0020a8daadd91c334580b9ea4a168
Mark Withall <github@freudiantypo.com>
2017-10-12 20:15:48 +0100

    Improve test name.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -6,7 +6,7 @@ describe('obfuscateText', () => {
     it('returns an empty string given an empty string', () => {
         expect(sample.ns.obfuscateText('')).toBe('');
     });
-    it('returns a single a for a single word', () => {
+    it('returns "a" given a single word', () => {
         expect(sample.ns.obfuscateText('hello')).toBe('a');
     });
     it('returns two "a a" given two words', () => {
