ba8e7df3089852d6bc4971b90325ba310a06c336
Mark Withall <github@freudiantypo.com>
2017-10-12 21:29:36 +0100

    Remove duplicate in function call.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,27 +1,27 @@
 'use strict';
 
-const sample = require('../index.js');
+const obfuscateText = require('../index.js').ns.obfuscateText;
 
 describe('obfuscateText', () => {
     it('returns an empty string given an empty string', () => {
-        expect(sample.ns.obfuscateText('')).toBe('');
+        expect(obfuscateText('')).toBe('');
     });
     it('returns "a" given a single word', () => {
-        expect(sample.ns.obfuscateText('hello')).toBe('a');
+        expect(obfuscateText('hello')).toBe('a');
     });
     it('returns "a" given a different single word', () => {
-        expect(sample.ns.obfuscateText('world')).toBe('a');
+        expect(obfuscateText('world')).toBe('a');
     });
     it('returns "a a" given two words', () => {
-        expect(sample.ns.obfuscateText('hello world')).toBe('a a');
+        expect(obfuscateText('hello world')).toBe('a a');
     });
     it('returns "a a a" given three words', () => {
-        expect(sample.ns.obfuscateText('foo bar baz')).toBe('a a a');
+        expect(obfuscateText('foo bar baz')).toBe('a a a');
     });
     it('return "a a" given two words separated by two spaces', () => {
-        expect(sample.ns.obfuscateText('hello  world')).toBe('a a');
+        expect(obfuscateText('hello  world')).toBe('a a');
     });
     it('returns "a a" given a single hyphenated word', () => {
-        expect(sample.ns.obfuscateText('hello-world')).toBe('a a');
+        expect(obfuscateText('hello-world')).toBe('a a');
     });
 });
