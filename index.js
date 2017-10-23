d739342b8267f6754acd2e78b82daa9d3c43125b
Mark Withall <github@freudiantypo.com>
2017-10-23 18:55:17 +0100

    Simplify replace using regex.

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ this.nanowrimo = this.nanowrimo || {};
 
 this.nanowrimo.obfuscateText = function (text) {
     return text
-        .replace('-', ' ')
-        .replace('\r', ' ')
-        .replace('\n', ' ')
+        .replace(/[-\r\n]/, ' ')
         .split(' ')
         .filter(function(x) { return x !== '' })
         .map(function() { return 'a' })
