c93f3005f50602f77b49a47cc6224806303dd002
Mark Withall <github@freudiantypo.com>
2017-10-23 19:00:46 +0100

    Simplify word separators.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ this.nanowrimo = this.nanowrimo || {};
 
 this.nanowrimo.obfuscateText = function (text) {
     return text
-        .replace(/[-\t\r\n]/, ' ')
+        .replace(/\W/, ' ')
         .split(' ')
         .filter(function(x) { return x !== '' })
         .map(function() { return 'a' })
