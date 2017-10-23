5812cb894d3c94fb6f643b81118562d4d792c53a
Mark Withall <github@freudiantypo.com>
2017-10-23 21:08:41 +0100

    Add count words function and tests.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const obfuscateText = require('../index.js').nanowrimo.obfuscateText;
+const countWords = require('../index.js').nanowrimo.countWords;
 
 describe('obfuscateText', () => {
     it('returns an empty string given an empty string', () => {
@@ -29,5 +30,35 @@ describe('obfuscateText', () => {
     });
     it('returns "a a" given words separated by tab', () => {
         expect(obfuscateText('hello\tworld')).toBe('a a');
+    });
+});
+
+describe('countWords', () => {
+    it('returns 0 given an empty string', () => {
+        expect(countWords('')).toBe(0);
+    });
+    it('returns 1 given a single word', () => {
+        expect(countWords('hello')).toBe(1);
+    });
+    it('returns 1 given a different single word', () => {
+        expect(countWords('world')).toBe(1);
+    });
+    it('returns 2 given two words', () => {
+        expect(countWords('hello world')).toBe(2);
+    });
+    it('returns 3 given three words', () => {
+        expect(countWords('foo bar baz')).toBe(3);
+    });
+    it('return 2 given two words separated by two spaces', () => {
+        expect(countWords('hello  world')).toBe(2);
+    });
+    it('returns 2 given a single hyphenated word', () => {
+        expect(countWords('hello-world')).toBe(2);
+    });
+    it('returns 2 given words separated by crlf', () => {
+        expect(countWords('hello\r\nworld')).toBe(2);
+    });
+    it('returns 2 given words separated by tab', () => {
+        expect(countWords('hello\tworld')).toBe(2);
     });
 });
